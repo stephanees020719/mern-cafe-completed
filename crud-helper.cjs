d69bc01d1cbd375058d10588fc36f5b5b2914b5d
stephanees020719 <stephanees020719@gmail.com>
2023-09-25 11:10:04 -0400

    completed mern-cafe-app

--- crud-helper.cjs
+++ crud-helper.cjs
@@ -3,15 +3,56 @@ require('dotenv').config();
 const db = require('./config/database.cjs');
 
 // Require the Mongoose models
-const User = require('./models/user.cjs');
-// const Item = require('./models/item');
-// const Category = require('./models/category');
-// const Order = require('./models/order');
+// const User = require('./models/user.cjs');
+// const Item = require('./models/item.cjs');
+// const Category = require('./models/category.cjs');
+const Order = require('./models/order.cjs');
 
 // Local variables will come in handy for holding retrieved documents
 let user, item, category, order;
 let users, items, categories, orders;
 
+Order.create({
+  user: '651099048d7137bb349e9278',
+  isPaid: true,
+  lineItems: [
+    {
+      qty: 5,
+      item: {
+        name: 'Hamburger',
+        emoji: '🍔',
+        price: 5.95,
+      },
+    },
+    {
+      qty: 2,
+      item: {
+        name: 'Turkey Sandwich',
+        emoji: '🥪',
+        price: 6.95,
+      },
+    },
+  ],
+})
+  .then((order) => {
+    console.log(order);
+  })
+  .finally(() => {
+    db.close();
+  });
+
+// {
+//   name: 'Hamburger',
+//   emoji: '🍔',
+//   category: categories[0]._id,
+//   price: 5.95,
+// },
+// {
+//   name: 'Turkey Sandwich',
+//   emoji: '🥪',
+//   category: categories[0]._id,
+//   price: 6.95,
+// },
 
 
 // user id to create a new user"651099048d7137bb349e9278"
